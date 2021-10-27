8b28736ebb21f0a3f881ac34a3836371955b7d29
albinalm <albin@codeindite.com>
2021-10-27 16:33:31 +0200

    Fixed button

--- src/application-insights-albinalm/application-insights-albinalm-web/Startup.cs
+++ src/application-insights-albinalm/application-insights-albinalm-web/Startup.cs
@@ -1,3 +1,4 @@
+using application_insights_albinalm_web.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace application_insights_albinalm_web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +52,7 @@ namespace application_insights_albinalm_web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<CalculatorHub>("/calculatorHub");
             });
         }
     }
